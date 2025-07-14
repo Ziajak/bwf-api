13cbf9cca169bb06dac8dfe58b6775d1024fa64e
Ziajak <radoslawmichalek86@gmail.com>
2025-07-14 16:22:31 +0200

    Adding urls, 001_initial

---
+++ api/urls.py
@@ -0,0 +1,12 @@
+from api import views
+from rest_framework import routers
+from django.conf.urls import include
+from django.urls import path
+
+router = routers.DefaultRouter()
+router.register(r'groups', views.GroupViewset)
+
+
+urlpatterns = [
+    path(r'^', include(router.urls)),
+]
