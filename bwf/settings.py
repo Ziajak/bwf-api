40d0471cc6418945a6f6dcc25b575df4a5dfcde8
Ziajak <radoslawmichalek86@gmail.com>
2025-07-30 17:09:55 +0200

    Adding corsheaders in settings

--- bwf/settings.py
+++ bwf/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'api'
 ]
 
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'bwf.urls'
@@ -70,6 +72,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'bwf.wsgi.application'
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
