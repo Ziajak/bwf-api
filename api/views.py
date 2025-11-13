00648ae4364676ca50847137cd3d56c0240baefc
Ziajak <radoslawmichalek86@gmail.com>
2025-11-13 12:29:36 +0100

    Endpoint - change password

--- api/views.py
+++ api/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
 from .models import Group, Event, UserProfile, User
 from .serializers import (GroupSerializer, EventSerializer, GroupFullSerializer,
-                          UserSerializer, UserProfileSerializer)
+                          UserSerializer, UserProfileSerializer, ChangePasswordSerializer)
 from rest_framework.response import Response
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
@@ -10,6 +11,20 @@ from rest_framework.authtoken.models import Token
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    @action(methods=['PUT'], detail=True, serializer_class=ChangePasswordSerializer)
+    def change_pass(self, request, pk):
+        user = User.objects.get(pk=pk)
+        serializer = ChangePasswordSerializer(data=request.data)
+
+        if serializer.is_valid():
+            if not user.check_password(serializer.data.get('old_password')):
+                return Response({'message': 'Wrong old password'},
+                                status=status.HTTP_400_BAD_REQUEST)
+            user.set_password(serializer.data.get('new_password'))
+            user.save()
+            return Response({'message': 'Password Updated'}, status.HTTP_200_OK)
+
 
 class UserProfileViewset(viewsets.ModelViewSet):
     queryset = UserProfile.objects.all()
