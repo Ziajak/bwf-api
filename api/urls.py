17cd52f8bde93a19d434094e6740372764fee518
Ziajak <radoslawmichalek86@gmail.com>
2025-12-17 15:31:54 +0100

    Adding urls comments

--- api/urls.py
+++ api/urls.py
@@ -8,6 +8,7 @@ router = routers.DefaultRouter()
 router.register(r'groups', views.GroupViewset)
 router.register(r'events', views.EventViewset)
 router.register(r'members', views.MemberViewset)
+router.register(r'comments', views.CommentViewset)
 router.register(r'users', views.UserViewSet)
 router.register(r'profile', views.UserProfileViewset)
 
