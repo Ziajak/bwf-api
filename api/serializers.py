981b75cb0dcff17b3bbc03ad0c1a7b8afee3ddde
Ziajak <radoslawmichalek86@gmail.com>
2026-02-11 13:45:52 +0100

    Modified serializers - UserSerializer. Handle create user without profile

--- api/serializers.py
+++ api/serializers.py
@@ -25,16 +25,18 @@ class UserProfileSerializer(serializers.ModelSerializer):
         model = UserProfile
         fields = ('id', 'image', 'is_premium', 'bio')
 class UserSerializer(serializers.ModelSerializer):
-    profile = UserProfileSerializer()
+    profile = UserProfileSerializer(required=False)
     class Meta:
         model = User
         fields = ('id', 'username', 'email', 'password', 'profile')
         extra_kwargs = {'password': {'write_only': True, 'required': False}}
 
     def create(self, validated_data):
-        profile_data = validated_data.pop('profile')
+        profile_data = validated_data.pop('profile', None)
         user = User.objects.create_user(**validated_data)
-        UserProfile.objects.create(user=user, **profile_data)
+        if profile_data:
+            UserProfile.objects.create(user=user, **profile_data)
+
         Token.objects.create(user=user)
         return user
 
